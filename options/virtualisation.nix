8ffa63bc09cb67b1da233202ae75ea0636257382
lunics <git.dev@lunics.fr>
2025-08-03 18:34:32 +0200

    remove virt option

--- options/virtualisation.nix
+++ options/virtualisation.nix
@@ -2,17 +2,7 @@
   options._ = {
     virtualisation = mkEnableOption "";
 
-    k3s = mkOption {
-      type    = types.bool;
-      default = config._.virtualisation;
-    };
-
     waydroid = mkOption {
-      type    = types.bool;
-      default = config._.virtualisation;
-    };
-
-    libvirt = mkOption {
       type    = types.bool;
       default = config._.virtualisation;
     };
